5a26f5f516786cfc85e9be056fb1f5155f6415be
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 12:34:26 +0100

    feat: apply redux provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
 import App from "./components/App";
+import rootReducer from "./reducers";
 
-render(<App />, document.getElementById("root"));
+const initialState = { categories: [] };
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
