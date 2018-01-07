00d07ca5326c22054df8ec974259d5209c590fab
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 16:14:18 +0100

    feat: connected to redux - button dispatches get cats action

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Button } from "reactstrap";
 import * as constants from "../constants";
 
 export class App extends Component {
   render() {
+    const { categories } = this.props;
     return (
       <div>
         <h2 className="display-1">{constants.mainTitle}</h2>
-        <Button className="btn-get-categories">{constants.GET_CATS_BTN}</Button>
+        <Button
+          className="btn-get-categories"
+          onClick={() =>
+            this.props.dispatch({ type: constants.types.GET_CATEGORIES })
+          }
+        >
+          {constants.GET_CATS_BTN}
+        </Button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    categories: state.categories
+  }),
+  null
+)(App);
