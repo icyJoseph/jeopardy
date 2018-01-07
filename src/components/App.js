4631109e598e6bd3b59dd6d754864a74fdea3bb6
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 11:49:10 +0100

    feat: display-1 applied to h2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>Welcome to Jeopardy</h2>
+        <h2 className="display-1">Welcome to Jeopardy</h2>
       </div>
     );
   }
