ab48deafe7d6e13d1429ab0c36af46cc1950bf7c
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 12:32:25 +0100

    feat: add action types

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,8 @@ Strings used as Titles and Subtitles
 
 export const mainTitle = "Welcome to Jeopardy";
 export const GET_CATS_BTN = "Get Jeopardy Categories";
+
+export const types = {
+  GET_CATEGORIES: "get_categories",
+  LOAD_CATEGORIES: "load_categories"
+};
