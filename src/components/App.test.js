4d937cd9fea15825c8b91dd0a5e39493fe333214
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 11:53:01 +0100

    test: component renders the mainTitle

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import * as constants from "../constants";
 
 describe("App", () => {
   let app = shallow(<App />);
@@ -11,5 +12,9 @@ describe("App", () => {
 
   it("has a display-1 heading", () => {
     expect(app.find(".display-1").exists()).toBe(true);
+  });
+
+  it("displays the `mainTitle` defined in constants", () => {
+    expect(app.find(".display-1").text()).toEqual(constants.mainTitle);
   });
 });
