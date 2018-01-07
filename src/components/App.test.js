d17a62b0861810e9cf0b734959c981361a7f6bd3
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 17:10:35 +0100

    test: verify correct action dispatch

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
+import sinon from "sinon";
 import { App } from "./App";
 import * as constants from "../constants";
 
 describe("App", () => {
-  let app = shallow(<App />);
+  const props = {
+    categories: [],
+    dispatch: sinon.spy()
+  };
+
+  let app = shallow(<App {...props} />);
 
   it("renders properly", () => {
     expect(app).toMatchSnapshot();
@@ -18,11 +24,21 @@ describe("App", () => {
     expect(app.find(".display-1").text()).toEqual(constants.mainTitle);
   });
 
-  describe("displays 20 categories of jeopardy", () => {
+  describe("displays categories of jeopardy", () => {
+    let getCatsBtn = app.find(".btn-get-categories");
     it("has a button to `GET` the categories", () => {
-      let getCatsBtn = app.find(".btn-get-categories");
       expect(getCatsBtn.exists()).toBe(true);
       expect(getCatsBtn.render().text()).toEqual(constants.GET_CATS_BTN);
+    });
+
+    describe("when clicked", () => {
+      const expectedAction = { type: constants.types.GET_CATEGORIES };
+
+      beforeEach(() => getCatsBtn.simulate("click"));
+
+      it("dispatched get_categories action", () => {
+        expect(props.dispatch.calledWith(expectedAction)).toBe(true);
+      });
     });
   });
 });
