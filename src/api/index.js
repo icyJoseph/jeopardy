b32b3245cc8db0730c91e4c97d51f8c7a4402ffc
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 13:41:37 +0100

    feat: return only categories data

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const apiEndPoint = `http://jservice.io/api/categories?count=20`;
 
 export const getCategories = () => {
-  return axios.get(apiEndPoint).then(response => response);
+  return axios.get(apiEndPoint).then(response => response.data);
 };
