2098fbd22d5fd91d8253491a6270512b0c7a941c
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 11:53:30 +0100

    feat: add mainTitle constant to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import * as constants from "../constants";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h2 className="display-1">Welcome to Jeopardy</h2>
+        <h2 className="display-1">{constants.mainTitle}</h2>
       </div>
     );
   }
